42ee03582ebe43b3bcac3c19dbc509a8b22edb20
Constantin Radu <radu17constantin@gmail.com>
2020-05-11 01:39:59 +0300

    rename components

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import SEO from '../components/seo';
 import Layout from '../components/layout';
-import HeroCarousel from '../components/HeroCarousel';
+import HeroCarousel from '../components/heroCarousel';
 
 const IndexPage = () => (
   <Layout>
