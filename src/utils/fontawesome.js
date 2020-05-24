6f958f4cdc6df6e1c51667a944af1d715214a85f
Constantin Radu <radu17constantin@gmail.com>
2020-05-24 09:45:09 +0300

    added fontAwesome icons & created library

---
+++ src/utils/fontawesome.js
@@ -0,0 +1,19 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+// Icons import
+import { faSearch, faRss, faCaretLeft, faCaretRight, faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
+
+library.add(
+  // General Icons
+  faSearch,
+  faRss,
+  faCaretLeft,
+  faCaretRight,
+  faAngleRight,
+
+  // Brands Icons
+  faFacebook,
+  faInstagram,
+  faTwitter,
+)
