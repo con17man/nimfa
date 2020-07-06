aa089e2f055070aa3b8d29ba9e866289f84b0bbd
Constantin Radu <radu17constantin@gmail.com>
2020-07-06 10:44:17 +0300

    goTo component hide imgBg onHover

--- src/components/QuickLinks.js
+++ src/components/QuickLinks.js
@@ -1,45 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'gatsby';
 
 import servicesImg from '../assets/images/quickLink_services.png';
 import productsImg from '../assets/images/quickLink_products.png';
+import industriesImg from '../assets/images/quickLink_industries.jpg';
 
 const goToSections = [
   {
-    name: 'Products',
+    name: 'Produse',
     url: '/products',
     img: productsImg
   },
   {
-    name: 'Services',
+    name: 'Servicii',
     url: '/services',
     img: servicesImg
   },
   {
-    name: 'Industries',
+    name: 'Industrie',
     url: '/industries',
-    img: ''
+    img: industriesImg
   }
 ];
 
-const QuickLinks = () => {
-  return(
-    <div className="quick-links-wrapper w-full">
-      {/* <div className="container mx-auto relative flex content-center items-center justify-center content-center text-center md:px-16 -mt-20"> */}
-      <div className="container mx-auto relative grid grid-flow-col grid-cols-3 text-center md:px-16 -mt-20">
-        {goToSections.map((section, i) => {
-          return  <Link to={section.url} key={i+1} className="block overflow-hidden relative">
-                    {
-                      section.img
-                        ? <img src={section.img} alt={section.name} className="duration-500 ease-in-out hover:scale-105 origin-center transform transition" />
-                        : <div className="h-full bg-blue"></div>
-                    }
-                    <p className={`absolute inset-1/2 flex items-center justify-center uppercase font-medium text-xl ${i%2 === 0 ? 'text-white': 'text-black'}`}>{section.name}</p>
-                  </Link>
-        })}
+
+
+class QuickLinks extends Component {
+
+  changeBG = (el, bgImg) => {
+    if (el.style.backgroundImage) {
+      el.style.backgroundImage = '';
+    } else {
+      el.style.backgroundImage = `url(${bgImg})`;
+    }
+  };
+
+  render() {
+    return(
+      <div className="quick-links-wrapper w-full">
+        <div className="container mx-auto relative grid grid-flow-col grid-cols-3 text-center md:px-16 -mt-20">
+          {goToSections.map((section, i) => {
+            return  <Link to={section.url} key={i+1} className="relative block overflow-hidden">
+                      <div className={`bg-cover bg-center py-56 hover:bg-blue hover:text-white uppercase font-medium text-xl ${i%2 === 0 ? 'text-white': 'text-black'}`}
+                          tabIndex={-1} role="link"
+                          style={{backgroundImage: `url(${section.img})`}}
+                          onFocus={(el) => this.changeBG(el.target, section.img)}
+                          onBlur={(el) => this.changeBG(el.target, section.img)}
+                          onMouseOut={(el) => this.changeBG(el.target, section.img)}
+                          onMouseOver={(el) => this.changeBG(el.target, section.img)}>
+                        {section.name}
+                      </div>
+                    </Link>
+          })}
+        </div>
       </div>
-    </div>
-  );
+    );
+  };
+
 };
 
 export default QuickLinks;
