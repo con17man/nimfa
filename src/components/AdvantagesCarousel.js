b111182be0fa301027a65823120f3b8f087c08da
Constantin Radu <radu17constantin@gmail.com>
2020-07-07 00:49:54 +0300

    refactor data - AdvantagesCarousel

--- src/components/AdvantagesCarousel.js
+++ src/components/AdvantagesCarousel.js
@@ -1,29 +1,7 @@
 import React, { Component } from 'react';
 import Slider from 'react-slick';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
-const advantages = [
-  {
-    title: 'Avantaj 1',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-  },
-  {
-    title: 'Avantaj 2',
-    description: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-  },
-  {
-    title: 'Avantaj 3',
-    description: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
-  },
-  {
-    title: 'Avantaj 4',
-    description: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-  },
-  {
-    title: 'Avantaj 5',
-    description: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-  }
-];
+import { useStaticQuery, graphql } from 'gatsby';
 
 
 const AdvantageItem = ({title, description}) => (
@@ -38,34 +16,44 @@ const AdvantageItem = ({title, description}) => (
 );
 
 
-export class AdvantagesCarousel extends Component {
-  render() {
+const AdvantagesCarousel = () => {
+  const queryData = useStaticQuery(graphql`
+    query advantagesQuery {
+      carousels {
+        advantagesCarousel {
+          description
+          title
+        }
+      }
+    }
+  `);
 
-    const sliderSettings = {
-      dots: false,
-      arrows: true,
-      infinite: false,
-      autoplay: false,
-      slidesToShow: 4,
-      slidesToScroll: 1
-    };
+  const advantages = queryData.carousels.advantagesCarousel;
 
-    return (
-      <div className="adv-wrapper w-full bg-grey -mt-16 pb-40">
-        <div className="adv-slider relative max-w-2xl mx-auto">
-          <Slider {...sliderSettings}>
-            {advantages.map((adv, i) => {
-              return  <AdvantageItem key={i+1}
-                        title={adv.title}
-                        description={adv.description}
-                        img={adv.img}>
-                      </AdvantageItem>
-            })}
-          </Slider>
-        </div>
+  const sliderSettings = {
+    dots: false,
+    arrows: true,
+    infinite: false,
+    autoplay: false,
+    slidesToShow: 4,
+    slidesToScroll: 1
+  };
+
+  return (
+    <div className="adv-wrapper w-full bg-grey -mt-16 pb-40">
+      <div className="adv-slider relative max-w-2xl mx-auto">
+        <Slider {...sliderSettings}>
+          {advantages.map((adv, i) => {
+            return  <AdvantageItem key={i+1}
+                      title={adv.title}
+                      description={adv.description}
+                      img={adv.img}>
+                    </AdvantageItem>
+          })}
+        </Slider>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default AdvantagesCarousel;
