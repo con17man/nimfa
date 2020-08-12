b2ec142191fa9eb58d3bf18fc310850ffcf22669
Constantin Radu <radu17constantin@gmail.com>
2020-08-13 01:06:31 +0300

    created advantages page structure with pageHero

--- src/pages/companie/avantaje.js
+++ src/pages/companie/avantaje.js
@@ -1,18 +1,46 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import { Link, useStaticQuery, graphql } from 'gatsby';
 
 import Layout from '../../components/layout';
 import SEO from '../../components/SEO';
+import PageHero from '../../components/PageHero';
 
-const AdvantagesPage = () => (
-  <Layout>
-    <SEO title="Avantaje" />
-    <div className="container mx-auto p-32 text-center">
-      <h1>Avantaje</h1>
-      <hr/>
-      <Link to="/">Go back to the homepage</Link>
-    </div>
-  </Layout>
-)
+const AdvantagesPage = () => {
+
+  const { pageAvantaje: { hero } } = useStaticQuery(graphql`
+    query queryAdvantagesPageData {
+      pageAvantaje {
+        hero {
+          title
+          headline
+          image {
+            childImageSharp {
+              fluid(maxWidth: 2000, toFormat: WEBP, grayscale: true) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+        advantages {
+          description
+          icon
+          title
+        }
+      }
+    }
+  `);
+
+  return (
+    <Layout>
+      <SEO title="Avantaje" />
+      <PageHero heroInfo={hero} />
+      <div className="container mx-auto p-32 text-center">
+        <h1>Avantaje</h1>
+        <hr/>
+        <Link to="/">Go back to the homepage</Link>
+      </div>
+    </Layout>
+  );
+}
 
 export default AdvantagesPage;
