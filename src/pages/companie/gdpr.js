b791407c2fc2412b0ed19069b196c014bc2cb288
Constantin Radu <radu17constantin@gmail.com>
2020-08-26 23:25:07 +0300

    GDPR page added abstract img & removed unnecessary imports

--- src/pages/companie/gdpr.js
+++ src/pages/companie/gdpr.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { useStaticQuery, graphql, Link } from 'gatsby';
+import { useStaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image';
 
 import Layout from '../../components/layout';
 import SEO from '../../components/SEO';
@@ -44,6 +45,11 @@ const GDPRPage = () => {
       <div className="w-full relative">
         <div className="container relative mx-auto py-24 px-8 md:px-16 lg:px-0">
           {gdprParagraphs.map((paragraph, i) => <div key={i+1} dangerouslySetInnerHTML={{ __html: paragraph }} /> )}
+        </div>
+
+        {/* abstract image */}
+        <div className="absolute bottom-0 right-0 opacity-25" style={{width: '36rem', zIndex: '-1', transform: 'rotateY(180deg)'}}>
+          { abstractImg && <Img fluid={abstractImg.childImageSharp.fluid} style={{transform: 'rotateY(180deg)'}} /> }
         </div>
       </div>
     </Layout>
