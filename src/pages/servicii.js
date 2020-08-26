869c9ef347feab47079dba8ce78ed3bfc1b4e279
Constantin Radu <radu17constantin@gmail.com>
2020-08-27 01:24:32 +0300

    Create Servicii Page

--- src/pages/servicii.js
+++ src/pages/servicii.js
@@ -1,45 +1,74 @@
 import React from 'react';
-// import { Link } from 'gatsby';
+import { useStaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image';
 
 import Layout from '../components/layout';
 import SEO from '../components/SEO';
 import PageHero from '../components/PageHero';
+import Asset from '../components/Asset';
 
-import image from '../assets/images/hp-hero/slider_hp_01.jpg';
+const ServicesPage = () => {
 
-const ServicesPage = () => (
-  <Layout>
-    <SEO title="Servicii" />
-    {/* HERO - START*/}
-    <PageHero heroInfo={{heroImg: image, title: 'hello', headline: 'Description'}} />
-    {/* HERO - END */}
+  const {pageServicii: {hero, services, abstractImg}} = useStaticQuery(graphql`
+    query queryServicesPageData {
+      pageServicii {
+        hero {
+          title
+          headline
+          image {
+            childImageSharp {
+              fluid(maxWidth: 2000, toFormat: WEBP) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
 
-    {/* DEMO - STICKY SCROLL */}
-    <div className="flex container relative mx-auto py-16 text-center h-screen">
-      <div className="w-1/2">
-        <div className="h-64 grid grid-cols-3 gap-2 text-white">
-          <div className="h-48 p-4 bg-blue hover:bg-orange">1</div>
-          <div className="h-48 p-4 bg-blue hover:bg-orange">1</div>
-          <div className="h-48 p-4 bg-blue hover:bg-orange">1</div>
-          <div className="h-48 p-4 bg-blue hover:bg-orange">1</div>
-          <div className="h-48 p-4 bg-blue hover:bg-orange">1</div>
-          <div className="h-48 p-4 bg-blue hover:bg-orange">1</div>
-          <div className="h-48 p-4 bg-blue hover:bg-orange">1</div>
-          <div className="h-48 p-4 bg-blue hover:bg-orange">1</div>
-          <div className="h-48 p-4 bg-blue hover:bg-orange">1</div>
-          <div className="h-48 p-4 bg-blue hover:bg-orange">1</div>
-          <div className="h-48 p-4 bg-blue hover:bg-orange">1</div>
+        services {
+          description
+          title
+          icon {
+            childImageSharp {
+              fluid(maxWidth: 192) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+
+        abstractImg {
+          childImageSharp {
+            fluid(maxWidth: 512) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  return (
+    <Layout>
+      <SEO title={hero.title} />
+      <PageHero heroInfo={hero} />
+      <div className="w-full relative">
+        <div className="container relative mx-auto grid grid-cols-1 lg:grid-cols-2 py-24 px-8 md:px-16 lg:px-0">
+          { services.map((service, i) => <div className="grid-flow-row-dense" key={`service_${i}`}><Asset assetData={service} /></div> )}
+        </div>
+
+        {/* TODO: Find a better way to split and correctly diplay info on page */}
+        {/* <div className="container relative mx-auto flex flex-wrap py-24 px-8 md:px-16 lg:px-0">
+          <div className="w-full lg:w-1/2 lg:pr-2">{services.filter((s, i) => !(i % 2)).map((service, i) => <Asset assetData={service} key={`service_l_${i}`} />)}</div>
+          <div className="w-full lg:w-1/2 lg:pl-2">{services.filter((s, i) => (i % 2)).map((service, i) => <Asset assetData={service} key={`service_r_${i}`} />)}</div>
+        </div> */}
+
+        {/* abstract image */}
+        <div className="absolute bottom-0 right-0 opacity-25" style={{width: '36rem', zIndex: '-1', transform: 'rotateY(180deg)'}}>
+          { abstractImg && <Img fluid={abstractImg.childImageSharp.fluid} style={{transform: 'rotateY(180deg)'}} /> }
         </div>
       </div>
-
-      <div className="w-1/2 relative" id="cos">
-        <div className="sticky top-28 p-4">
-         <h3>Header</h3>
-         <p>Lorem ipsum</p>
-        </div>
-      </div>
-    </div>
-  </Layout>
-)
+    </Layout>
+  );
+}
 
 export default ServicesPage;
