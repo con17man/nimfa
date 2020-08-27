7ccdd1351e28637c08296a4753c0db52ccba1387
Constantin Radu <radu17constantin@gmail.com>
2020-08-27 10:51:23 +0300

    small fixes on GDPR & Services Pages

--- src/pages/servicii.js
+++ src/pages/servicii.js
@@ -52,15 +52,15 @@ const ServicesPage = () => {
       <SEO title={hero.title} />
       <PageHero heroInfo={hero} />
       <div className="w-full relative">
-        <div className="container relative mx-auto grid grid-cols-1 lg:grid-cols-2 py-24 px-8 md:px-16 lg:px-0">
-          { services.map((service, i) => <div className="grid-flow-row-dense" key={`service_${i}`}><Asset assetData={service} /></div> )}
-        </div>
-
         {/* TODO: Find a better way to split and correctly diplay info on page */}
-        {/* <div className="container relative mx-auto flex flex-wrap py-24 px-8 md:px-16 lg:px-0">
+        {/* <div className="container relative mx-auto grid grid-cols-1 lg:grid-cols-2 py-24 px-8 md:px-16 lg:px-0">
+          { services.map((service, i) => <div className="grid-flow-row-dense" key={`service_${i}`}><Asset assetData={service} /></div> )}
+        </div> */}
+
+        <div className="container relative mx-auto flex flex-wrap py-24 px-8 md:px-16 lg:px-0">
           <div className="w-full lg:w-1/2 lg:pr-2">{services.filter((s, i) => !(i % 2)).map((service, i) => <Asset assetData={service} key={`service_l_${i}`} />)}</div>
           <div className="w-full lg:w-1/2 lg:pl-2">{services.filter((s, i) => (i % 2)).map((service, i) => <Asset assetData={service} key={`service_r_${i}`} />)}</div>
-        </div> */}
+        </div>
 
         {/* abstract image */}
         <div className="absolute bottom-0 right-0 opacity-25" style={{width: '36rem', zIndex: '-1', transform: 'rotateY(180deg)'}}>
