914e44b0e0c5b8e95b8ff994363ffbb1b34573ac
Constantin Radu <radu17constantin@gmail.com>
2020-08-31 11:55:36 +0300

    created products page grid menu

--- src/pages/produse.js
+++ src/pages/produse.js
@@ -7,7 +7,7 @@ import SEO from '../components/SEO';
 import PageHero from '../components/PageHero';
 
 const ProductsPage = () => {
-  const {pageProduse: {hero, categories: productCategories}} = useStaticQuery(graphql`
+  const {pageProduse: {hero, productCategories}} = useStaticQuery(graphql`
     query queryProductsPageData {
       pageProduse {
         hero {
@@ -32,25 +32,27 @@ const ProductsPage = () => {
               }
             }
           }
+          gridPosition
         }
 
       }
     }
   `);
 
-  // const [tevi, flanse, fittings, tablaProfile, accesorii, armaturiRobineti] = productCategories;
   return (
     <Layout>
       <SEO title={hero.title} />
       <PageHero heroInfo={hero} />
-      <div className="relative container mx-auto -mb-8 p-32 bg-grey">
-        <div className="grid grid-rows-2 grid-flow-col gap-2 text-center">
-          <div className="row-start-1 border-2 col-span-2 border-red-500">1 Tevi</div>
-          <div className="row-start-2 border-2 col-span-2 border-red-500">2 Flanse</div>
-          <div className="col-span-2 border-2 border-red-500">3 Fitting-uri</div>
-          <div className="border-2 border-red-500">4 Tabla si profile</div>
-          <div className="border-2 border-red-500">5 Accesorii</div>
-          <div className="row-span-2 border-2 border-red-500">6 Armaturi si robineti</div>
+      <div className="relative container mx-auto -mb-8 p-28 bg-grey">
+        <div className="grid-container">
+          {productCategories.map((category, i) => {
+            return <div key={i+1} className={category.gridPosition}>
+              <Link to={category.url}>
+                <Img fluid={category.img.childImageSharp.fluid} className="object-cover w-full h-full" />
+                <p className="area-title">{category.name}</p>
+              </Link>
+            </div>
+          })}
         </div>
       </div>
     </Layout>
