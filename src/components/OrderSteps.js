e5f4eee538e4379a39a085f5343fc0f4a9fa9d6d
Constantin Radu <radu17constantin@gmail.com>
2020-08-31 09:01:18 +0300

    Added final order steps

--- src/components/OrderSteps.js
+++ src/components/OrderSteps.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Img from 'gatsby-image';
 
 const OrderSteps = () => {
   const queryData = useStaticQuery(graphql`
@@ -10,7 +10,13 @@ const OrderSteps = () => {
           description
           title
           steps {
-            icon
+            icon {
+              childImageSharp {
+                fluid(maxWidth: 192) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
             id
             name
           }
@@ -26,11 +32,13 @@ const OrderSteps = () => {
       <div className="steps-list container mx-auto text-center">
         <p className="list-title font-montserrat-alternates font-semibold text-4xl tracking-wider">{orderSteps.title}</p>
         <p className="list-description font-light mx-auto max-w-2xl py-2">{orderSteps.description}</p>
-        <div className="list-items flex mx-auto max-w-2xl">
+        <div className="list-items flex justify-center mx-auto max-w-3xl">
           {orderSteps.steps.map((step, i) => {
-            return  <div key={i+1}
-                      className="list-item adv-item-img h-32 w-32 my-6 rounded-full shadow-xl mx-auto bg-orange flex justify-center items-center text-5xl text-white duration-300 ease-in-out transition">
-                        <FontAwesomeIcon icon={step.icon.split(',')} />
+            return  <div key={i+1} className="px-6">
+                      <div className="list-item adv-item-img h-32 w-32 my-6 rounded-full shadow-xl mx-auto bg-orange flex justify-center items-center text-5xl text-white duration-300 ease-in-out transition">
+                          <Img fluid={step.icon.childImageSharp.fluid} className="w-24" />
+                      </div>
+                      <p className="uppercase font-medium tracking-wide">{step.name}</p>
                     </div>
           })}
         </div>
