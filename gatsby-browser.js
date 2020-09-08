cb32c1429e124a0777e31722f081d8dc84ab97b1
Constantin Radu <radu17constantin@gmail.com>
2020-09-08 03:16:19 +0300

    fixed scroll position aboveheader

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -12,3 +12,29 @@ import './src/styles/main.css';
 
 import 'slick-carousel/slick/slick.css';
 import "slick-carousel/slick/slick-theme.css";
+
+
+// Fix scroll position for anchor tag ids
+// https://chaseohlson.com/gatsby-link-anchor-navigation
+
+export const onRouteUpdate = ({location}) => {
+  anchorScroll(location);
+  return true;
+};
+
+export const shouldUpdateScroll = ({
+  routerProps: { location },
+  getSavedScrollPosition
+}) => {
+  anchorScroll(location);
+  return true;
+}
+
+function anchorScroll(location){
+  // Check for location so build does not fail
+  if (location && location.hash) {
+    setTimeout(() => {
+      window.scrollTo({top: window.scrollY - 100, left: 0, behavior: 'smooth'});
+    }, 0);
+  }
+}
