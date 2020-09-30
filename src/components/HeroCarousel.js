dc622afd01bb83373f5f40d607c8a8a950b5d174
Constantin Radu <radu17constantin@gmail.com>
2020-09-30 11:50:48 +0300

    extended spacing to fix issue with heroCarousel img issue

--- src/components/HeroCarousel.js
+++ src/components/HeroCarousel.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Link, useStaticQuery, graphql } from 'gatsby';
 import Img from 'gatsby-image';
 import Slider from 'react-slick';
@@ -8,28 +8,11 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Hero = ({ title, link, linkLabel, img, linkCSS }) => {
 
-  const innerWidth = typeof window !== 'undefined' && window.innerWidth;
-  const matchMedia = typeof window !== 'undefined' && window.matchMedia;
-
-  const [mQuery, setMQuery] = useState({
-    matches: innerWidth < 1024 ? true : false
-  });
-
-  useEffect(() => {
-    let mediaQuery = matchMedia("(min-width: 1024px)");
-    mediaQuery.addEventListener('change', () => {
-      mediaQuery.addListener(setMQuery);
-    });
-
-    // this is the cleanup function to remove the listener
-    return () => mediaQuery.removeListener(setMQuery);
-  }, [matchMedia]);
-
   return (
   <div className="relative">
     <div className="relative " style={{paddingBottom: '510px'}}> {/* pb-3/6 lg:pb-1/2 xl:pb-4/12 */}
       <div className="absolute object-cover w-full h-full">
-        <Img fluid={img.childImageSharp.fluid} alt={`Nimfa - ${title}`} style={{paddingBottom: mQuery.matches ? '65%' : 'unset'}} />
+        <Img fluid={img.childImageSharp.fluid} alt={`Nimfa - ${title}`} className="pb-138 lg:pb-0" />
       </div>
     </div>
 
@@ -62,7 +45,7 @@ const HeroCarousel = () => {
           linkCSS
           img {
             childImageSharp {
-              fluid(maxWidth: 2000, toFormat: WEBP) {
+              fluid(maxWidth: 2000, toFormat: PNG) {
                 ...GatsbyImageSharpFluid
               }
             }
